7bfc858faae197dc8b82ffc496f98e17aac935e7
name <abc@def.com>
2024-10-04 17:14:13 +0900

    useState 사용

--- m2/Practice/src/App.jsx
+++ m2/Practice/src/App.jsx
@@ -1,30 +1,26 @@
 import './App.css'
-import List from './components/List'
+import { useState } from 'react';
 
 function App() {
-
-  const nickname = '이노'
-  const name = '장예원'
-  const likeWhat = ['음악', '음식']
-
-  const artists = ['Post Malone', 'Nothing But Thieves', 'Lany', 'ColdPlay', 'Cigarettes After Sex']
-  const foods = ['곱창', '불족발', '아인슈페너', '콜드브루']
-
+  const [cnt1, setCnt1] = useState(0)
+  const [cnt2, setCnt2] = useState(0)
+  
+  
+  const handleIncreaseNumber1 = () => {
+    setCnt1(cnt1+1);
+  }
+  
+  const handleIncreaseNumber2 = () => {
+    setCnt2(cnt2 => cnt2+1);
+    setCnt2(cnt2 => cnt2+1);
+  }
   return (
     <>
-      <strong className='school'>서경대학교</strong>
-      <p style={{color: 'blue', fontWeight: 'bold', fontSize:'3rem'}}> {nickname}/{name} </p>
-      <h1>{ `${nickname}는 ${likeWhat}을 좋아합니다.`}</h1>
-      <ul>
-        {artists.map((artist, idx) => (
-          <List key={idx} artistName={artist} />
-        ))}
-      </ul>
-      <ul>
-        {foods.map((food, idx) => (
-          <List key={idx} foodName={food} />
-        ))}
-      </ul>
+      <h1>{cnt1}</h1>
+      <button onClick={handleIncreaseNumber1}>1 증가</button>
+
+      <h1>{cnt2}</h1>
+      <button onClick={handleIncreaseNumber2}>2 증가</button>
     </>
   )
 }
