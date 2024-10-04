7f08fbe41a870d4e81daf04ccf6863f3d8f1feaf
name <abc@def.com>
2024-10-04 16:10:13 +0900

    JSX 사용시 유의사항(기초)

---
+++ m2/Practice/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <strong className='school'>서경대학교</strong>
+      <p style={{color: 'blue', fontWeight: 'bold', fontSize:'3rem'}}> 이노/장예원 </p>
+    </>
+  )
+}
+
+export default App
