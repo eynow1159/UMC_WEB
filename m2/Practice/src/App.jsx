667a94d592ce2ff10e7c3d92534065d6e34b2a24
name <abc@def.com>
2024-10-04 16:44:44 +0900

    첫 컴포넌트 만들어보기

--- m2/Practice/src/App.jsx
+++ m2/Practice/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 
+// 1. List Component를 Import
+import List from './components/List'
+
 function App() {
 
   const nickname = '이노'
   const name = '장예원'
   const likeWhat = '음악'
 
-  const artists = ['Postmalone', 'Nothing But Thieves', 'Lany', 'ColdPlay', 'Cigarettes After Sex']
+  const artists = ['Post Malone', 'Nothing But Thieves', 'Lany', 'ColdPlay', 'Cigarettes After Sex']
 
   return (
     <>
@@ -14,9 +17,10 @@ function App() {
       <p style={{color: 'blue', fontWeight: 'bold', fontSize:'3rem'}}> {nickname}/{name} </p>
       <h1>{ `${nickname}는 ${likeWhat}을 좋아합니다.`}</h1>
       <ul>
-        {artists.map((artist, idx) => {
-          return <li key={idx}>{artist}</li>
-        })}
+        {artists.map((artist, idx) => (
+          // 2. <li key={idx}>{artist}</li> 를 List 컴포넌트 호출로 변경
+          <List key={idx} artistName={artist} />
+        ))}
       </ul>
     </>
   )
---
+++ m2/Practice/src/components/List.jsx
@@ -0,0 +1,10 @@
+const List = (props) => {
+    console.log(props)
+    return (
+        <li>
+            {props.artistName}
+        </li>
+    )
+}
+
+export default List
