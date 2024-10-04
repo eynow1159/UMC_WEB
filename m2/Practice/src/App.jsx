a0b218b73050a50d8ca6d15c75026cb6fd2c8ef0
name <abc@def.com>
2024-10-04 16:26:58 +0900

    배열의 요소를 나타내는 방법

--- m2/Practice/src/App.jsx
+++ m2/Practice/src/App.jsx
@@ -2,10 +2,22 @@ import './App.css'
 
 function App() {
 
+  const nickname = '이노'
+  const name = '장예원'
+  const likeWhat = '음악'
+
+  const artists = ['Postmalone', 'Nothing But Thieves', 'Lany', 'ColdPlay', 'Cigarettes After Sex']
+
   return (
     <>
       <strong className='school'>서경대학교</strong>
-      <p style={{color: 'blue', fontWeight: 'bold', fontSize:'3rem'}}> 이노/장예원 </p>
+      <p style={{color: 'blue', fontWeight: 'bold', fontSize:'3rem'}}> {nickname}/{name} </p>
+      <h1>{ `${nickname}는 ${likeWhat}을 좋아합니다.`}</h1>
+      <ul>
+        {artists.map((artist, idx) => {
+          return <li key={idx}>{artist}</li>
+        })}
+      </ul>
     </>
   )
 }
