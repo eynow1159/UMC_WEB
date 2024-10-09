61af1446d2e37f6ac04eda934984d658116b894f
name <abc@def.com>
2024-10-10 02:50:54 +0900

    에러처리 주석 추가

--- m3/Practice2/src/App.jsx
+++ m3/Practice2/src/App.jsx
@@ -3,6 +3,11 @@ import './App.css'
 // 1. import를 해줍니다.
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
+/*
+(1) 유저가 이상한 경로에 접근했을 떄, 홈으로 이동시킬 수 있는 버튼을 제작. (UX)
+(2) 각 페이지 별 다른 에러 처리
+(3) ErrorBoundary 처리
+*/
 const router = createBrowserRouter([
     {
         path: '/',
