d7ecb4b1a1b9457f276063fd74b4186a4ae73f51
name <abc@def.com>
2024-11-01 04:53:47 +0900

    ~4주차 폴더 생성

--- m3/Practice2/src/App.jsx
+++ m3/Practice2/src/App.jsx
@@ -1,22 +1,16 @@
 import './App.css'
 
+// 1. import를 해줍니다.
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
-// 1. 만든 페이지들을 import
-import HomePage from "./pages/home.jsx";
-import NotFound from "./pages/not-found.jsx";
-import Movies from "./pages/movies.jsx";
-
-// 2. 연결
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <HomePage/>,
-        errorElement: <NotFound/>
+        element: <h1>홈 페이지입니다.</h1>
     },
     {
         path: '/movies',
-        element: <Movies/>
+        element: <h1>영화 페이지 입니다.</h1>
     }
 ])
 
---
+++ m4/Practice1/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import CustomButton from "./components/custom-button.jsx";
+
+function App() {
+    return (
+        <>
+            <CustomButton/>
+        </>
+    )
+}
+
+export default App
